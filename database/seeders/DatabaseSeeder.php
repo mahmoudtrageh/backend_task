e0311f74bee782d502d71246c372c53fe5d1c34c
mahmoudtrageh <geo.mahmoudtaha@gmail.com>
2025-04-14 22:33:55 +0200

    fixes: fix seeder issue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             DepartmentSeeder::class,
             PositionSeeder::class,
             LeaveTypeSeeder::class,
-            UserDepartnentPositioneeder::class,
+            UserDepartmentPositionSeeder::class,
             LeaveRequestSeeder::class,
         ]);
     }
